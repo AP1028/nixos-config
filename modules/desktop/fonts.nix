69d9107901c2206d9aeba7a3e532bf0b4c2a1920
Tianyi Xia <tian_yi_xia@qq.com>
2026-06-24 22:08:20 +0800

    Auto-commit from rebuild (asusg16): 2026-06-24 22:08:20

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -5,24 +5,39 @@
   ...
 }: let
   harmonyos = pkgs.callPackage ../../packages/harmonyos-sans-font.nix {};
-in {
-  fonts.packages = with pkgs; [
-    corefonts
-    vista-fonts
 
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    sarasa-gothic
-    harmonyos
-    wqy_zenhei
-  ];
+  # Import your new local package file safely using conditional evaluation
+  ms-fonts =
+    if builtins.pathExists ../../git-excluded/fonts/msfonts.zip
+    then pkgs.callPackage ../../packages/ms-fonts.nix {}
+    else null;
+in {
+  # Accept the unfree Microsoft font licenses
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "corefonts"
+      "vista-fonts"
+    ];
+
+  fonts.packages = with pkgs;
+    [
+      corefonts
+      vista-fonts
+
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      sarasa-gothic
+      harmonyos
+      wqy_zenhei
+    ]
+    ++ (lib.optional (ms-fonts != null) ms-fonts); # Only include if file exists
 
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
-      sansSerif = ["HarmonyOS Sans" "HarmonyOS Sans SC"];
-      serif = ["Noto Serif" "Noto Serif CJK SC"];
+      sansSerif = ["Microsoft YaHei" "HarmonyOS Sans" "HarmonyOS Sans SC"];
+      serif = ["SimSun" "Noto Serif" "Noto Serif CJK SC"];
       monospace = ["Sarasa Gothic"];
       emoji = ["Noto Color Emoji"];
     };
