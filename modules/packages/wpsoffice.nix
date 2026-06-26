87cd3adcd00f218397327e2d85a8c2841cde8e89
Tianyi Xia <tian_yi_xia@qq.com>
2026-06-26 16:45:05 +0800

    Auto-commit from rebuild (asusg16): 2026-06-26 16:45:05

---
+++ modules/packages/wpsoffice.nix
@@ -0,0 +1,24 @@
+{pkgs, ...}: let
+  wps-wrapped = pkgs.symlinkJoin {
+    name = "wpsoffice-cn-zh";
+    paths = [pkgs.wpsoffice-cn];
+    buildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      for bin in $out/bin/*; do
+        wrapProgram "$bin" \
+          --set LANG zh_CN.UTF-8 \
+          --set LC_ALL zh_CN.UTF-8
+      done
+
+      rm $out/share/applications/*.desktop
+      cp ${pkgs.wpsoffice-cn}/share/applications/*.desktop $out/share/applications/
+      chmod +w $out/share/applications/*.desktop
+      sed -i "s|^Exec=.*|Exec=$out/bin/wps %f|" $out/share/applications/wps-office-wps.desktop
+      sed -i "s|^Exec=.*|Exec=$out/bin/et %f|" $out/share/applications/wps-office-et.desktop
+      sed -i "s|^Exec=.*|Exec=$out/bin/wpp %f|" $out/share/applications/wps-office-wpp.desktop
+      sed -i "s|^Exec=.*|Exec=$out/bin/wpspdf %f|" $out/share/applications/wps-office-wpspdf.desktop
+    '';
+  };
+in {
+  environment.systemPackages = [wps-wrapped];
+}
