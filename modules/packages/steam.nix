bd6c2f4b75ab1422d7ea1f6a52002e8ad3e9a559
Tianyi Xia <tian_yi_xia@qq.com>
2026-06-26 15:05:51 +0800

    Auto-commit from rebuild (asusg16): 2026-06-26 15:05:51

--- modules/packages/steam.nix
+++ modules/packages/steam.nix
@@ -3,13 +3,17 @@
   inputs,
   ...
 }: {
+  nixpkgs.overlays = [
+    inputs.nix-gaming-edge.overlays.proton-cachyos
+  ];
+
   # Steam with Proton GE and CJK font support inside the runtime
   programs.steam = {
     enable = true;
 
     extraCompatPackages = with pkgs; [
       proton-ge-bin
-      inputs.nix-proton-cachyos.packages.${system}.proton-cachyos
+      proton-cachyos
     ];
     remotePlay.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
