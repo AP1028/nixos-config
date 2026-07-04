251b0ab92b1c324bfc3600a4433b95765033949d
Tianyi Xia <tian_yi_xia@qq.com>
2026-07-05 00:27:48 +0800

    Auto-commit from rebuild (asusg16): 2026-07-05 00:27:48

--- hosts/macbook/system/default.nix
+++ hosts/macbook/system/default.nix
@@ -1,16 +1,6 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{...}: {
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nixpkgs.config.allowUnfree = true;
-
-  fonts.packages = with pkgs; [
-    dejavu_fonts
-    corefonts
-  ];
 
   system.stateVersion = "25.11";
 }
