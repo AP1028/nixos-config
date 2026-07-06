27b3add05eb530905acd6526f913afde69e94719
Tianyi Xia <tian_yi_xia@qq.com>
2026-07-06 23:21:12 +0800

    Auto-commit from rebuild (macbook): 2026-07-06 23:21:12

--- modules/services/audio.nix
+++ modules/services/audio.nix
@@ -3,18 +3,21 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  # 32-bit x86 package set only exists on the x86 family.
+  supports32Bit = pkgs.stdenv.hostPlatform.isx86;
+in {
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa.support32Bit = supports32Bit;
     pulse.enable = true;
   };
 
-  environment.systemPackages = with pkgs.pkgsi686Linux; [
+  environment.systemPackages = lib.optionals supports32Bit (with pkgs.pkgsi686Linux; [
     alsa-lib
     pipewire
     libpulseaudio
-  ];
+  ]);
 }
