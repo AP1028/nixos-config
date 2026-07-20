c710d63a92ac67b5fa38b6471b38f07f8b5d1ae0
Tianyi Xia <tian_yi_xia@qq.com>
2026-07-20 17:03:55 +0800

    sillytavern: use proper module options (port, persistent configFile)

--- hosts/nixos-service-vm/services/sillytavern.nix
+++ hosts/nixos-service-vm/services/sillytavern.nix
@@ -6,6 +6,8 @@
 }: {
   services.sillytavern = {
     enable = true;
+    port = 8000;
+    configFile = "/var/lib/SillyTavern/config-persistent.yaml";
   };
   networking.firewall.allowedTCPPorts = [8000];
 }
