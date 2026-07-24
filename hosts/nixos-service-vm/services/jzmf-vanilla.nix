89d9fa03f582e8c14ea1908dc2b48076fbdd00b7
Tianyi Xia <tian_yi_xia@qq.com>
2026-07-24 19:57:10 +0800

    nixos-service-vm: rename jzmf-vanilla dir, add jzmf-construction service (Temurin 21)

--- hosts/nixos-service-vm/services/jzmf-vanilla.nix
+++ hosts/nixos-service-vm/services/jzmf-vanilla.nix
@@ -5,7 +5,7 @@
 }: {
   systemd.services = {
     jzmf-vanilla = {
-      description = "jzmf-vanilla-26.2 Minecraft Server (Temurin 25) in Tmux";
+      description = "jzmf-vanilla Minecraft Server (Temurin 25) in Tmux";
       after = ["network.target"];
       wantedBy = ["multi-user.target"];
       path = [
@@ -18,7 +18,7 @@
         Type = "forking";
         User = "service";
         Group = "users";
-        WorkingDirectory = "/home/service/jzmf-vanilla-26.2";
+        WorkingDirectory = "/home/service/jzmf-vanilla";
         ExecStartPre = "${pkgs.bash}/bin/bash -c '${pkgs.tmux}/bin/tmux kill-session -t jzmf-vanilla 2>/dev/null || true'";
         ExecStart = "${pkgs.tmux}/bin/tmux new-session -d -s jzmf-vanilla ./run.sh";
         ExecStop = "${pkgs.tmux}/bin/tmux send-keys -t jzmf-vanilla 'stop' C-m";
