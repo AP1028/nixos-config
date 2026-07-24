44cc5a5583c6ff3c02236adbf6b9678e5ced9503
Tianyi Xia <tian_yi_xia@qq.com>
2026-07-24 20:08:00 +0800

    all 3 mc servers: Type=oneshot, wrap run.sh in while loop for crash restart

--- hosts/nixos-service-vm/services/helloneojournautics.nix
+++ hosts/nixos-service-vm/services/helloneojournautics.nix
@@ -17,15 +17,15 @@ in {
         pkgs.coreutils
       ];
       serviceConfig = {
-        Type = "forking";
+        Type = "oneshot";
+        RemainAfterExit = true;
         User = "service";
         Group = "users";
         WorkingDirectory = "/home/service/HelloNeoJournautics";
         ExecStartPre = "${pkgs.bash}/bin/bash -c '${pkgs.tmux}/bin/tmux kill-session -t mc-server 2>/dev/null || true'";
-        ExecStart = "${pkgs.tmux}/bin/tmux new-session -d -s mc-server ./run.sh";
+        ExecStart = "${pkgs.tmux}/bin/tmux new-session -d -s mc-server '${pkgs.bash}/bin/bash -c \"while true; do ./run.sh; sleep 5; done\"'";
         ExecStop = "${pkgs.tmux}/bin/tmux send-keys -t mc-server 'stop' C-m";
         TimeoutStopSec = 120;
-        Restart = "always";
       };
     };
   };
--- hosts/nixos-service-vm/services/jzmf-vanilla.nix
+++ hosts/nixos-service-vm/services/jzmf-vanilla.nix
@@ -15,15 +15,15 @@
         pkgs.coreutils
       ];
       serviceConfig = {
-        Type = "forking";
+        Type = "oneshot";
+        RemainAfterExit = true;
         User = "service";
         Group = "users";
         WorkingDirectory = "/home/service/jzmf-vanilla";
         ExecStartPre = "${pkgs.bash}/bin/bash -c '${pkgs.tmux}/bin/tmux kill-session -t jzmf-vanilla 2>/dev/null || true'";
-        ExecStart = "${pkgs.tmux}/bin/tmux new-session -d -s jzmf-vanilla ./run.sh";
+        ExecStart = "${pkgs.tmux}/bin/tmux new-session -d -s jzmf-vanilla '${pkgs.bash}/bin/bash -c \"while true; do ./run.sh; sleep 5; done\"'";
         ExecStop = "${pkgs.tmux}/bin/tmux send-keys -t jzmf-vanilla 'stop' C-m";
         TimeoutStopSec = 120;
-        Restart = "always";
       };
     };
   };
