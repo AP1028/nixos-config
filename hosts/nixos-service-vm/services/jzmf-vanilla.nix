b326878759dd7d82ac9fed0d21d66f015a0d7eed
Tianyi Xia <tian_yi_xia@qq.com>
2026-07-25 17:58:57 +0800

    debug: temporarily run run.sh directly without screen to capture env and output

--- hosts/nixos-service-vm/services/jzmf-vanilla.nix
+++ hosts/nixos-service-vm/services/jzmf-vanilla.nix
@@ -8,11 +8,11 @@
       pkgs.temurin-bin-25
     ];
     serviceConfig = {
-      Type = "forking";
+      Type = "simple";
       User = "service";
       Group = "users";
       WorkingDirectory = "/home/service/jzmf-vanilla";
-      ExecStart = "${pkgs.screen}/bin/screen -dmS jzmf-vanilla ./run.sh";
+      ExecStart = "${pkgs.bash}/bin/bash -c 'echo START > /tmp/vanilla-debug.log; env >> /tmp/vanilla-debug.log; pwd >> /tmp/vanilla-debug.log; ls -la ./run.sh >> /tmp/vanilla-debug.log 2>&1; echo \"--- running run.sh ---\" >> /tmp/vanilla-debug.log; ./run.sh >> /tmp/vanilla-debug.log 2>&1; echo \"exit: $?\" >> /tmp/vanilla-debug.log'";
       ExecStop = "${pkgs.screen}/bin/screen -p 0 -S jzmf-vanilla -X eval 'stuff \"stop\"\\015'";
       Environment = "TERM=xterm-256color";
       Restart = "always";
