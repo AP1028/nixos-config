e12d35b8d788e575584dadb85058cfcb60aa016c
Tianyi Xia <tian_yi_xia@qq.com>
2026-07-25 17:58:57 +0800

    debug: temporarily run run.sh directly without screen to capture env and output

--- hosts/nixos-service-vm/services/helloneojournautics.nix
+++ hosts/nixos-service-vm/services/helloneojournautics.nix
@@ -11,11 +11,11 @@ in {
       ysmJava
     ];
     serviceConfig = {
-      Type = "forking";
+      Type = "simple";
       User = "service";
       Group = "users";
       WorkingDirectory = "/home/service/HelloNeoJournautics";
-      ExecStart = "${pkgs.screen}/bin/screen -dmS helloneojournautics ./run.sh";
+      ExecStart = "${pkgs.bash}/bin/bash -c 'echo START > /tmp/hnj-debug.log; env >> /tmp/hnj-debug.log; pwd >> /tmp/hnj-debug.log; ls -la ./run.sh >> /tmp/hnj-debug.log 2>&1; echo \"--- running run.sh ---\" >> /tmp/hnj-debug.log; ./run.sh >> /tmp/hnj-debug.log 2>&1; echo \"exit: $?\" >> /tmp/hnj-debug.log'";
       ExecStop = "${pkgs.screen}/bin/screen -p 0 -S helloneojournautics -X eval 'stuff \"stop\"\\015'";
       Environment = "TERM=xterm-256color";
       Restart = "always";
