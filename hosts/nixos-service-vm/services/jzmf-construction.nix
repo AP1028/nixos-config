ad84e31d15aeba9ae35515407bf185d568ac6de6
Tianyi Xia <tian_yi_xia@qq.com>
2026-07-25 17:27:40 +0800

    fix: restore pkgs.screen in service path, use store path

--- hosts/nixos-service-vm/services/jzmf-construction.nix
+++ hosts/nixos-service-vm/services/jzmf-construction.nix
@@ -4,6 +4,7 @@
     after = ["network.target"];
     wantedBy = ["multi-user.target"];
     path = [
+      pkgs.screen
       pkgs.temurin-bin-21
     ];
     serviceConfig = {
@@ -11,8 +12,8 @@
       User = "service";
       Group = "users";
       WorkingDirectory = "/home/service/jzmf-construction";
-      ExecStart = "screen -DmS jzmf-construction ./run.sh";
-      ExecStop = "screen -p 0 -S jzmf-construction -X eval 'stuff \"stop\"\\015'";
+      ExecStart = "${pkgs.screen}/bin/screen -DmS jzmf-construction ./run.sh";
+      ExecStop = "${pkgs.screen}/bin/screen -p 0 -S jzmf-construction -X eval 'stuff \"stop\"\\015'";
       Restart = "always";
       RestartSec = 15;
       TimeoutStopSec = 120;
