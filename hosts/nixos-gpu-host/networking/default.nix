38603046c041e5f960707acc402f439a75b37f48
Tianyi Xia <tian_yi_xia@qq.com>
2026-08-15 20:57:43 +0800

    networking: 40G interconnect (10.0.0.0/24, mtu 9000) for gpu-host and gpu-vm

--- hosts/nixos-gpu-host/networking/default.nix
+++ hosts/nixos-gpu-host/networking/default.nix
@@ -3,14 +3,24 @@
   networking.networkmanager.enable = false;
   networking.useDHCP = false;
 
-  # The old 7700k machine used enp3s0. Adjust this to the physical NIC name
-  # after first boot if the replacement hardware differs.
-  networking.interfaces.enp3s0.ipv4.addresses = [
+  # Gigabit management NIC
+  networking.interfaces.eno1.ipv4.addresses = [
     {
       address = "192.168.3.200";
       prefixLength = 24;
     }
   ];
+
+  # 40G interconnect to pve (vmbr1, 10.0.0.1/24): jumbo frames + dedicated subnet
+  networking.interfaces.enp133s0 = {
+    mtu = 9000;
+    ipv4.addresses = [
+      {
+        address = "10.0.0.200";
+        prefixLength = 24;
+      }
+    ];
+  };
 
   networking.defaultGateway = "192.168.3.1";
   networking.nameservers = ["192.168.3.1"];
--- hosts/nixos-gpu-vm/networking/default.nix
+++ hosts/nixos-gpu-vm/networking/default.nix
@@ -10,6 +10,17 @@
     }
   ];
 
+  # 40G bridge (vmbr1) on the Proxmox host: jumbo frames + dedicated subnet
+  networking.interfaces.ens19 = {
+    mtu = 9000;
+    ipv4.addresses = [
+      {
+        address = "10.0.0.103";
+        prefixLength = 24;
+      }
+    ];
+  };
+
   networking.defaultGateway = "192.168.3.1";
   networking.nameservers = ["192.168.3.1"];
 }
