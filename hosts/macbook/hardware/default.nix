1cfba538bbf893eec62c158e78e2defba3427a60
Tianyi Xia <tian_yi_xia@qq.com>
2026-08-25 22:40:19 +0000

    Auto-commit from rebuild (macbook): 2026-08-25 22:40:19

--- hosts/macbook/hardware/default.nix
+++ hosts/macbook/hardware/default.nix
@@ -11,9 +11,6 @@
     "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
   ];
 
-  boot.kernelModules = ["88x2bu"];
-  boot.extraModulePackages = [config.boot.kernelPackages.rtl88x2bu];
-
   hardware.asahi.enable = true;
   hardware.asahi.peripheralFirmwareDirectory = /. + "${config.local.configDir}/hosts/macbook/firmware";
 
